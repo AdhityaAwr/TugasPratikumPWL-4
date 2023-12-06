c9a4a7dab41a0dcdfcf466930f605b963931a149
Adhitya <herogg212@gmail.com>
2023-12-06 16:37:34 +0700

    Laravel breeze

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,15 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                </div> 
+                @hasrole('pustakawan')
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('role')" :active="request()->routeIs('role')">
+                        {{ __('Role') }}
+                    </x-nav-link>
                 </div>
+                @endhasrole
+                
             </div>
 
             <!-- Settings Dropdown -->
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::middleware('auth')->group(function () {
+    Route::view('/roles', 'role')->name('role')->middleware(['role:pustakawan']);
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
